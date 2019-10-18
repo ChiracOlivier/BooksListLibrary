e80bd713ae17a629a948c74d9ddb5cae4e5a57e0
Chiracolivier <chiracolivier28@gmail.com>
2019-10-18 03:25:10 +0200

    arranging header.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BooksListLibrary';
+  constructor() {
+
+    const firebaseConfig = {
+      apiKey: 'AIzaSyCPY6oxWvHHtSg54uM70SbkUqABC_IPf2A',
+      authDomain: 'bookslistlibrary-59027.firebaseapp.com',
+      databaseURL: 'https://bookslistlibrary-59027.firebaseio.com',
+      projectId: 'bookslistlibrary-59027',
+      storageBucket: 'bookslistlibrary-59027.appspot.com',
+      messagingSenderId: '512436408604',
+      appId: '1:512436408604:web:067a7038ce40f6084b6ad9',
+      measurementId: 'G-KLX4QTSNJG'
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators} from '@angular/forms';
+import {AuthService} from '../../services/auth.service';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+
+  signUpForm: FormGroup;
+  errorMessage: string;
+
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private router: Router) {}
+
+
 
   ngOnInit() {
+    this.InitForm();
+  }
+    matchValues(
+    matchTo: string // name of the control to match to
+  ): (AbstractControl) => ValidationErrors | null {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return !!control.parent &&
+      !!control.parent.value &&
+      control.value === control.parent.controls[matchTo].value
+        ? null
+        : { isMatching: false };
+    };
   }
 
+  InitForm() {
+
+    this.signUpForm = this.formBuilder.group({
+      prenom: ['', [Validators.required]],
+      nom: ['', [Validators.required]],
+      email : ['', [Validators.required, Validators.email]],
+      password: ['', [ Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]],
+      password2: ['', [ Validators.required, this.matchValues('password')]],
+    });
+  }
+  onSubmit() {
+    const email = this.signUpForm.get('email').value;
+    const password = this.signUpForm.get('password').value;
+    this.authService.createNewUser(email, password).then(
+      () => {
+        this.router.navigate(['books']);
+      },
+      (error) => {
+        this.errorMessage = error;
+      }
+    );
+  }
+
+  onSubm() {
+    const email = this.signUpForm.get('email').value;
+    const password = this.signUpForm.get('password').value;
+    this.authService.createNewUser(email, password).then(
+      () => {
+        this.router.navigate(['books']);
+      },
+      (error) => {
+        this.errorMessage = error;
+      }
+    );
+  }
 }
